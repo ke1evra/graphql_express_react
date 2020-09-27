44ec9a50d1080f6e11fb4d0042c7a69f3e50fee8
ke1eva <dolgov.konstantin@gmail.com>
2020-09-27 22:36:40 +0300

    image vertical align

--- client/src/components/postItem.jsx
+++ client/src/components/postItem.jsx
@@ -4,10 +4,13 @@ export default function PostItem({post}) {
     return (
 
           <div className="row mb-3">
-              <div className="col border border-dark rounded py-3">
-                  <div className="row">
+              <div className="col">
+                  <div className="row border border-dark rounded py-3 mx-0">
                       <div className="col-md-3">
+                        <div className="h-100 d-flex align-items-center">
                           <img className="img-fluid" src={post.img} alt=""/>
+                        </div>
+
                       </div>
                       <div className="col-md-9">
                           <h3>{post.title}</h3>
